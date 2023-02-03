c2e4c679428191bb95f3e28c07a41affa606f6d9
sawonhossain34 <sawonhossain3491@gmail.com>
2023-02-03 12:48:23 +0600

    array cheack and typeof and concat

---
+++ array.js
@@ -0,0 +1,23 @@
+const country = "Bangladesh";
+const age = 25;
+const inIndependent = true;
+const student = {id:121, class:11, name:'Agun'};
+const friends = [12,21,54,52,77,14,]
+
+console.log(typeof country);
+console.log(typeof age);
+console.log(typeof inIndependent);
+console.log(typeof student);
+
+
+const newFriendsAge =[10,20,30,40,];
+const allFriends = newFriendsAge.concat(friends);
+console.log(allFriends);
+
+//cheack array 
+console.log(Array.isArray(friends));
+
+
+
+
+
