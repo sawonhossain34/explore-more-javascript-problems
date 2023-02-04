b7ef572fc7bdded711d7de25ad5eb1c095ccbb6c
sawonhossain34 <sawonhossain3491@gmail.com>
2023-02-04 11:39:03 +0600

    while loop odd number 7-19

---
+++ let-const.js
@@ -0,0 +1,8 @@
+// let & const apply the change 
+
+const name = 'sawon hossain';
+let age = 25;
+
+for( age = 25 ; age <= 26 ; age++){
+    console.log('I am ', age, 'years old.');
+}
---
+++ loop.js
@@ -0,0 +1,37 @@
+// // loop all <,>,==,!=,<=,>=
+
+// // const sawon = 50;
+// // const alamin = 60;
+// // const fahim = 70;
+
+// // if(sawon > alamin && sawon > fahim){
+// //     console.log('sawon first ');
+// // }else if(alamin > sawon && alamin > fahim){
+// //     console.log('alamin winner');
+// // }else{
+// //     console.log('fahim winner');
+// }
+// ///////////
+
+// const signUpName = 'sawon hossain';
+// const password = 1213573379;
+
+
+// const loginName = 'sawon hossain';
+// const loginPassword = 121357337;
+
+// if(signUpName === loginName && password === loginPassword){
+//     console.log('you are a correct information');
+// }else('worng information');
+
+// if(signUpName === loginName || password === loginPassword){
+//     console.log('you are a correct information');
+// }else('worng information');
+
+// while loop
+
+let i = 7;
+while(i <= 19){
+console.log(i);
+i+=2;
+}
